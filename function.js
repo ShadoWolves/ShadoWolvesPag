24a88106c099c77852eadab4a0881e09dfb69697
Gigo <rodrigigomendoza@gmail.com>
2023-06-21 14:19:37 -0400

    primer commit

---
+++ function.js
@@ -0,0 +1,26 @@
+// -------TRANSICIÓN---------
+
+$(document).ready(function () {
+    $("a").click(function () {
+        var gato = this.hash
+        $("html, body").animate({
+                scrollTop: $(gato).offset().top -10
+            },
+            100
+        )
+
+    })
+
+
+})
+
+
+// ---------TOOLTIPS---------
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+        boundary: "window",
+        template:
+            '<div class="tooltip tooltip-custom" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+    })
+})
+
